a84838d82cadb5dcb5587d661280ed2aae66da2e
陶思淇 <thinklf97@gmail.com>
2022-12-08 14:02:49 +0800

    init

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,9 +1,17 @@
 <template>
-<!--  <div @click="router.push('print')">去打印</div>-->
-  <div @click="router.push('photo')">去拍照</div>
-  <div @click="router.push('video')">去拍视频</div>
-  <div @click="router.push('audio')">去录音</div>
-  <p>拍照、视频仅支持手机端，录音都支持</p>
+  <!--  <div @click="router.push('print')">去打印</div>-->
+  JavaScrip Api在安卓上存在较多兼容性问题
+  <div @click="router.push('photo')">JS-去拍照</div>
+
+  <div @click="router.push('video')">JS-去拍视频</div>
+  <div @click="router.push('audio')">JS-去录音</div>
+
+  JsSdk均是调用的安卓IOS原生功能，仅在企业微信以及钉钉环境有效
+  <div><a href="https://open.work.weixin.qq.com/api/jsapidemo#menu-image">企业微信JsSdk-拍照</a></div>
+  <div><a href="https://open.work.weixin.qq.com/api/jsapidemo#menu-voice">企业微信JsSdk-录音</a></div>
+  <div><a href=" https://open.work.weixin.qq.com/api/jsapidemo#menu-scan">企业微信JsSdk-扫码</a></div>
+
+  <div><a href="https://dev-apsmanage.rspaas.cn/#/demo">钉钉JsSdk-拍照&录音&扫码</a></div>
 </template>
 
 <script setup>
@@ -12,13 +20,17 @@ import router from "@/router";
 
 <style scoped>
 div {
-  width: 100px;
+  /*width: 100px;*/
   height: 50px;
   background: green;
   color: aliceblue;
   text-align: center;
   line-height: 50px;
   font-size: 26px;
-  margin: 50px;
+  margin: 20px;
+}
+a{
+  color: #fff;
+  text-decoration-line: none;
 }
 </style>
